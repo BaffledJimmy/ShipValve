b816f0c9b701b404180e6210a1025f3b456a96d6
BaffledJimmy <tommac2005@gmail.com>
2017-07-30 17:58:21 +0100

    Rename and tweaked GPIO syntax.
    Next : add menu to allow user to pick how many x100 litres to pump (ie how many seconds to have the solenoid open for)

--- pumpwater.py
+++ pumpwater.py
@@ -5,35 +5,33 @@ import pygame
 
 pygame.mixer.init()
 pygame.mixer.music.load("Siren.wav")
-
 GPIO.setmode(GPIO.BCM)
 
 print "Welcome the RN Ballast Control System.  Unauthorised access is prohibited."
 
 # Create list with pin numbers used in script
-
 pinList = [2, 3, 4, 17]
 
-for i in pinList: GPIO.setup(i, GPIO.OUT) GPIO.output(i, GPIO.HIGH)
+# Set up GPIO on each pin number (FOR loop)
+for i in pinList:
+    GPIO.setup(i, GPIO.OUT), GPIO.output(i, GPIO.HIGH)
 
 # This variable (in seconds) determines how long the solenoid valves will be open and how long water will flow.
-
 SleepTimeL = 10
 
 # Loop
 
+# Change this to the pin where your solenoid is connected on your Pi GPIO board.
 try:
-# Change this to the pin where your solenoid is connected on your Pi GPIO
-
-GPIO.output(17, GPIO.LOW) 
-time.sleep(SleepTimeL);
-GPIO.cleanup()
-print "Ship Ballast Valve Opened!"
-pygame.mixer.Sound("Siren.mp3")
+    GPIO.output(17, GPIO.LOW)
+    time.sleep(SleepTimeL)
+    GPIO.cleanup()
+    print "Ship Ballast Valve Opened!"
+    pygame.mixer.Sound("Siren.mp3")
 
 # End program cleanly with keyboard
 except KeyboardInterrupt:
-print " Exitting"
+    print " Exitting"
 
 # Reset GPIO settings
 GPIO.cleanup()
